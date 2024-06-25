01edf14997d659391a590db9eb0de11de49e98dc
Fabian Barua <baruafabian@gmail.com>
2024-06-25 16:32:57 -0400

    feat: Agregar sección de mezcladoras en MixSection.tsx

--- src/components/MixSection.tsx
+++ src/components/MixSection.tsx
@@ -1,6 +1,32 @@
 import { motion } from 'framer-motion'
 import { PrimaryButton } from './PrimaryButton'
 
+const mixers = [
+  {
+    id: 'mixteraSection1',
+    tittle: 'Code',
+    image: '/home/mixteraSection/1.png',
+    url: '/mixteras',
+    className: ' translate-x-[-50%] left-[30%] top-[55%] -translate-y-1/2  -rotate-12'
+
+  },
+  {
+    id: 'mixteraSection2',
+    tittle: 'Code',
+    image: '/home/mixteraSection/2.png',
+    url: '/mixteras',
+    className: ' translate-x-[-50%] left-[50%] z-[99999] top-[40%] -translate-y-1/2 '
+  },
+  {
+    id: 'mixteraSection3',
+    tittle: 'Code',
+    image: '/home/mixteraSection/3.png',
+    url: '/mixteras',
+    className: ' translate-x-[-50%] left-[70%] top-[55%] -translate-y-1/2  rotate-12'
+
+  }
+]
+
 export const MixSection: React.FC = () => {
   return (
     <>
@@ -13,9 +39,9 @@ export const MixSection: React.FC = () => {
           hidden: { opacity: 0, y: 40 }
         }}
         transition={{ duration: 0.5, delay: 0.2 }}
-        className=' bg-interbrasGray  rounded-3xl h-[30rem] flex items-center px-24 '
+        className=' bg-interbrasGray  rounded-3xl h-[30rem] flex items-center pl-24 '
       >
-        <div className=' text-white flex flex-col gap-3 '>
+        <div className=' text-white flex flex-col gap-3  w-[50%] '>
           <h2 className=' text-5xl font-semibold '>
             Mixteras &
             <br />
@@ -29,6 +55,24 @@ export const MixSection: React.FC = () => {
           <div className=' w-min'>
             <PrimaryButton to='/mixteras' />
           </div>
+        </div>
+        <div className=' relative  w-full   h-full'>
+          {mixers.map((mixer) => (
+            <div
+              key={mixer.id} className={
+              `
+              absolute rounded-3xl  shadow-xl  bg-white flex justify-center items-center size-64
+              ${mixer.className}
+              `
+            }
+            >
+              <img
+                src={mixer.image}
+                alt={mixer.tittle}
+                className=' rounded-3xl '
+              />
+            </div>
+          ))}
         </div>
       </motion.section>
     </>
