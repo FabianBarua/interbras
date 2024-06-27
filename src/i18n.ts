de1449bc5a9685d32debbe251487c368eda422a1
Fabian Barua <baruafabian@gmail.com>
2024-06-27 12:35:33 -0400

    feat: Update i18n initialization to remove unnecessary await

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18next from 'i18next'
 import es from './shared/translations/es/global.json'
 import pt from './shared/translations/pt/global.json'
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18next.init({
   interpolation: {
     escapeValue: false
