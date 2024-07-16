cc6eb9fbe321ad08194f3dce7a18c5a5aa9c774a
Fabian Barua <baruafabian@gmail.com>
2024-07-16 09:37:08 -0400

    chore: Actualizar traducciones en global.json

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,8 +8,21 @@ import { CocinasSection } from '../components/CocinasSection'
 import { HervidorasAndCafeterasSection } from '../components/HervidorasAndCafeterasSection'
 import { OtherProductsSection } from '../components/OtherProductsSection.tsx'
 import { motion } from 'framer-motion'
+import { DownloadAlert } from '../components/DownloadAlert.tsx'
+import { useTranslation } from 'react-i18next'
+
+// verify Android or iOS
 
 export const Home: React.FC = () => {
+  const isAndroid: () => boolean = () => {
+    const ua = navigator.userAgent.toLowerCase()
+    return ua.includes('android')
+  }
+  const { t } = useTranslation('global')
+
+  const AndroidUrl = 'http://html5.zhouchangshu.com/apk/huihao.apk'
+  const IosUrl = 'https://apps.apple.com/es/app/id1509887773'
+
   return (
     <>
       <motion.div
@@ -17,6 +30,29 @@ export const Home: React.FC = () => {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.3 }}
       >
+
+        <DownloadAlert
+          Title={() => (
+            <p className=' flex items-center gap-3'>
+              <img
+                src='/emojis/scooter.png'
+                alt='Download App'
+                className=' size-5 '
+              />
+              {
+                t('alert.scooters.title')
+              }
+            </p>
+          )}
+          message={
+            t('alert.scooters.message')
+          }
+          link={
+            (isAndroid())
+              ? AndroidUrl
+              : IosUrl
+          }
+        />
 
         <ProductsAnimated />
         <main className='  sm:w-[80%] w-[90%] flex flex-col gap-6 mx-auto'>
