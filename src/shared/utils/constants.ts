eac2b46773966a8892d17a7dd2d7c73d5b8a704b
kingston-developer <cuentas@tclmobile.com.py>
2024-07-18 16:00:48 -0400

    feat: Actualizar URL de descarga de la aplicación Android de Scooter

--- src/shared/utils/constants.ts
+++ src/shared/utils/constants.ts
@@ -3,5 +3,5 @@ export const isApple: () => boolean = () => {
   return ua.includes('iphone') || ua.includes('ipad') || ua.includes('ipod') || ua.includes('mac')
 }
 
-export const ScooterAndroidUrl = 'https://html5.zhouchangshu.com/apk/huihao.apk'
+export const ScooterAndroidUrl = 'https://github.com/kingston-developer/interbrasApps/raw/main/scooter.apk'
 export const ScooterIosUrl = 'https://apps.apple.com/es/app/id1509887773'
