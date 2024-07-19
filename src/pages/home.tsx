2590df0e429d6300a393534c05fd383850fa9288
kingston-developer <cuentas@tclmobile.com.py>
2024-07-19 14:37:42 -0400

    remove download alert

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,16 +8,11 @@ import { CocinasSection } from '../components/CocinasSection'
 import { HervidorasAndCafeterasSection } from '../components/HervidorasAndCafeterasSection'
 import { OtherProductsSection } from '../components/OtherProductsSection.tsx'
 import { motion } from 'framer-motion'
-import { DownloadAlert } from '../components/DownloadAlert.tsx'
-import { useTranslation } from 'react-i18next'
 import { Helmet } from 'react-helmet'
-import { isApple, ScooterAndroidUrl, ScooterIosUrl } from '../shared/utils/constants.ts'
 
 // verify Android or iOS
 
 export const Home: React.FC = () => {
-  const { t } = useTranslation('global')
-
   return (
     <>
 
@@ -30,29 +25,6 @@ export const Home: React.FC = () => {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.3 }}
       >
-
-        <DownloadAlert
-          Title={() => (
-            <p className=' flex items-center gap-3'>
-              <img
-                src='/emojis/scooter.png'
-                alt='Download App'
-                className=' size-5 '
-              />
-              {
-                t('alert.scooters.title')
-              }
-            </p>
-          )}
-          message={
-            t('alert.scooters.message')
-          }
-          link={
-            (isApple())
-              ? ScooterIosUrl
-              : ScooterAndroidUrl
-          }
-        />
 
         <ProductsAnimated />
         <main className='  sm:w-[80%] w-[90%] flex flex-col gap-6 mx-auto'>
